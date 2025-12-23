b6caab4eabc4663bea7ade9904682f674bc395e3
Reza Akmal <rezajakmal@gmail.com>
2025-12-23 18:31:07 +0700

    feat: add database migrations for all entities

---
+++ migrations/1766399080332_create-table-songs.js
@@ -0,0 +1,36 @@
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    genre: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'INTEGER',
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      references: '"albums"',
+      onDelete: 'CASCADE',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
