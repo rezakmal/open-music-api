34da86d89c12b184225ef175a9e83159b6ffa3a0
Reza Akmal <rezajakmal@gmail.com>
2025-12-26 23:19:43 +0700

    reindent lines

--- open-music-api/src/middleware/auth.js
+++ open-music-api/src/middleware/auth.js
@@ -5,26 +5,26 @@ const TokenManager = require('../tokenize/TokenManager');
 const authMiddleware = (req, res, next) => {
   try {
     const { authorization } = req.headers;
-
+    
     if (!authorization) {
       throw new AuthenticationError('Missing authentication');
     }
-
+    
     const token = authorization.split(' ')[1];
     
     if (!token) {
-        throw new AuthenticationError('Missing authentication');
+      throw new AuthenticationError('Missing authentication');
     }
-
+    
     const { id } = TokenManager.verifyAccessToken(token);
     req.user = { id };
     next();
   } catch (error) {
-      if (error instanceof ClientError) {
-          next(error);
-      } else {
-          next(new AuthenticationError('Access token tidak valid'));
-      }
+    if (error instanceof ClientError) {
+      next(error);
+    } else {
+      next(new AuthenticationError('Access token tidak valid'));
+    }
   }
 };
 
